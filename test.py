fe92990411fefb7091e10caf43ce8e6ecc3cd11b
lrnholanda <lrnholanda@gmail.com>
2021-03-06 16:14:44 -0300

    outros arquivos

---
+++ test.py
@@ -0,0 +1,12 @@
+pizzas = ['portuguesa', 'mexicana', 'quatro queijos', 'caxiense']
+for pizza in pizzas:
+    print(f"I like {pizza.title()} pizza!")
+
+print("I really love pizza, that is my favorite food!")  
+
+animals = ['papagaio', 'gato', 'cachorro']
+for animal in animals:
+    print(f"A {animal.title()} would make a great pet.")
+
+print("todos são fofos :3")    
+  
